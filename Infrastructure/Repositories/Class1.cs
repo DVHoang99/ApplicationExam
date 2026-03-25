b74ba8f250e4d49d25fa55a99a5b1fea76c32608
Dao Van Hoang <dvhoang080699@gmail.com>
2026-03-25 17:29:18 +0700

    maning

--- Infrastructure/Repositories/Class1.cs
+++ Infrastructure/Repositories/Class1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Infrastructure.Persistence.Repositories
+namespace Infrastructure.Repositories
 {
     internal class Class1
     {
