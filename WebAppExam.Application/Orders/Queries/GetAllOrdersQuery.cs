92fd0fe34ffb23cbb3b8fff9283b55c69e324ad8
Dao Van Hoang <dvhoang080699@gmail.com>
2026-03-30 08:51:58 +0700

    chore: refactor

--- WebAppExam.Application/Orders/Queries/GetAllOrdersQuery.cs
+++ WebAppExam.Application/Orders/Queries/GetAllOrdersQuery.cs
@@ -4,8 +4,6 @@ using WebAppExam.Application.Common.Caching;
 using WebAppExam.Application.Orders.DTOs;
 
 namespace WebAppExam.Application.Orders.Queries;
-
-[CacheQuery(prefix: "orders_list", expirationMinutes: 5)]
 public class GetAllOrdersQuery(string customerName) : IRequest<List<OrderDto>>
 {
     public string CustomerName { get; set; } = customerName;
